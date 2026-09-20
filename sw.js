ce6455afe4a7e93a0f5c9bdc290b652b783d5ee9
Jawatheappbuilder <ashleyjpemberton@gmail.com>
2026-09-20 20:26:24 +0930

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='volyft-v5';const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
