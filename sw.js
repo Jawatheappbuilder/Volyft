964750fc006a5e894b38a1e86111ffba9947e417
Jawatheappbuilder <ashleyjpemberton@gmail.com>
2026-09-20 20:28:51 +0930

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='volyft-v8';const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
